be0fdba4e358f31a2e7653849a7460d2ac04f487
kwon <63184686+Kwondongkyun@users.noreply.github.com>
2022-01-11 19:51:31 +0900

    Create dfs.java

---
+++ BFS, DFS/dfs.java
@@ -0,0 +1,9 @@
+public static void dfs(int i) {
+    visit[i] = true;
+    System.out.print(i + " ");
+    for(int j=1; j<n+1; j++) {
+        if(map[i][j] == 1 && visit[j] == false) {
+            dfs(j);
+        }
+    }
+}
